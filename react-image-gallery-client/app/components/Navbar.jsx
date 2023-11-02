b5d72121a073c0952b11ed49efa4fd02826ecf3b
Amdadul Islam <amdadullahrayhan@gmail.com>
2023-11-03 00:36:46 +0600

    sovled responsivenes problem on make Featured button

--- react-image-gallery-client/app/components/Navbar.jsx
+++ react-image-gallery-client/app/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useImages } from "../contexts/ImagesContext";
+import { FaTrash } from "react-icons/fa6";
 const Navbar = ({
   selectedImages,
   handleMakeFeatured,
@@ -9,38 +10,40 @@ const Navbar = ({
 }) => {
   const [images] = useImages();
   return (
-    <div className="px-4 lg:px-8 py-4 bg-white border-b flex justify-between h-16">
-      <div className="flex items-center gap-4 font-bold">
-        <div className="flex  items-center gap-2 ">
-          <input
-            type="checkbox"
-            checked={selectedImages.length > 0 && "checked"}
-            className="checkbox"
-            onChange={toggleSelectAll}
-          />
-          {selectedImages.length > 0
-            ? selectedImages.length === images.length
-              ? "All"
-              : selectedImages.length
-            : "No"}{" "}
-          {selectedImages.length > 1 ? "Files" : "File"} Selected
-        </div>
+    <div className="px-4 lg:px-8 py-5 bg-white border-b flex justify-between  ">
+      <div className="flex  items-center gap-1 sm:gap-2  font-bold">
+        <input
+          type="checkbox"
+          checked={selectedImages.length > 0 && "checked"}
+          className="checkbox"
+          onChange={toggleSelectAll}
+        />
+        {selectedImages.length > 0
+          ? selectedImages.length === images.length
+            ? "All"
+            : selectedImages.length
+          : "No"}{" "}
+        {selectedImages.length > 1 ? "Files" : "File"} Selected
       </div>
-      {selectedImages.length === 1 && !alreadyFeatured && (
-        <button
-          className="btn btn-sm text-green-500"
-          onClick={() => handleMakeFeatured("")}
-        >
-          Make Featured
-        </button>
-      )}
+
+      <div>
+        {selectedImages.length === 1 && !alreadyFeatured && (
+          <button
+            className="btn btn-xs sm:btn-sm text-green-500"
+            onClick={() => handleMakeFeatured("")}
+          >
+            Make Featured
+          </button>
+        )}
+      </div>
       <div>
         <button
           onClick={handleDeleteSelected}
-          className="text-rose-400 font-bold disabled:text-rose-200"
+          className="ml-12 sm:ml-0 text-rose-400 font-bold disabled:text-rose-200"
           disabled={selectedImages.length < 1 && true}
         >
-          Delete {selectedImages.length > 1 ? "Files" : "File"}
+          <FaTrash className="text-xl" />
+          {/* Delete {selectedImages.length > 1 ? "Files" : "File"} */}
         </button>
       </div>
     </div>
